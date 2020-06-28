ff2b52d1e7b5d8b7ae47cdd875c5e4841d99bcd2
kimura <mkimura7878@gmail.com>
2020-06-28 12:00:13 +0000

    Groupモデルを作成した

---
+++ app/controllers/api/v1/groups_controller.rb
@@ -0,0 +1,54 @@
+module Api
+  module V1
+    class GroupsController < ApiController
+      before_action :authenticate_user!
+      before_action :set_group, only: [:show, :update, :destroy]
+
+
+      def index
+        groups = Group.where(user_id: current_user.id)
+        render json: groups
+      end
+
+      def show
+        render json: @group
+      end
+
+      def create
+        @group = Group.new(group_params)
+        @group.user_id = current_user.id
+        if @group.save
+          render json: @group, status: :created
+        else
+          render json: { errors: @group.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        if @group.update_attributes(group_params)
+          head :no_content
+        else
+          render json: { errors: @group.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        if @group.destroy
+          head :no_content
+        else
+          render json: @group.errors, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+        def set_group
+          @group = Group.find(params[:id])
+        end
+
+        def group_params
+          params.require(:group).permit(:name, :user_id)
+        end
+    end
+  end
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
+
+  namespace :api, {format: 'json'} do
+    namespace :v1 do
+      resources :groups
+    end
+  end
 end
