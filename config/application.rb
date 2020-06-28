a3baff5550341fd35da2102fde33f934002419ed
kimura <mkimura7878@gmail.com>
2020-06-28 08:13:57 +0000

    Homeコントローラーを作成した

--- config/application.rb
+++ config/application.rb
@@ -21,11 +21,16 @@ module Myapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.time_zone = 'Asia/Tokyo'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
 
     # Don't generate system test files.
     config.generators.system_tests = nil
